4aad35f66b355b7d86e54f35d08fcfb9aafc3c7f
akimzet <akim18@horizon.csueastbay.edu>
2016-12-29 22:10:59 -0800

    Finished First Tutorial

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//Gets the difference from the camera position and the player position
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//Camera's position is now the players position + the difference we calculated
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    //Called before rendering a frame
+	void Update(){}
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	//Counts each pickup
+	private int count;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText();
+		winText.text = "";
+	}
+
+    //Called before just performing any physics calculations
+    void FixedUpdate()
+    {
+		//Use Ctrl + ' To get documentation
+		//Gets axis from input
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce(movement * speed);
+    }
+
+	//When collision occurs
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Pick Up"))
+		{
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+		if (count >= 10)
+		{
+			winText.text = "You Win";
+		}
+	}
+}
